d399707e617745679785e01a77a7ba24295761b0
jgrynczewski <jgrynczewski@gmail.com>
2023-04-17 21:01:19 +0200

    add product create view

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,5 @@ from mainapp import views
 
 urlpatterns = [
     path('list/', views.ProductListView.as_view(), name='list_view'),
+    path('create/', views.ProductCreateView.as_view(), name='create-view'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from mainapp.models import Product
 
@@ -7,3 +8,9 @@ from mainapp.models import Product
 # Create your views here.
 class ProductListView(ListView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy('list_view')
